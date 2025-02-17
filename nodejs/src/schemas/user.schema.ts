ecf4ed8bdc40a85b09a8a187c5f35c0f1c3b5d49
Dries Verelst <dries.verelst@unikoo.be>
2025-02-17 18:52:27 +0100

    feat: restart project

--- nodejs/src/schemas/user.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Type, Static } from "@sinclair/typebox";
-
-export const UserSchema = Type.Object({
-  id: Type.String({ format: "uuid" }),
-  name: Type.String(),
-  email: Type.String({ format: "email" }),
-});
-
-export type User = Static<typeof UserSchema>;
